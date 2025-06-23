21b86dd6c1addd2846d0dda69e0d8affe0e5ed41
Monisha <missmonishavimala@gmail.com>
2025-06-24 00:21:19 +0530

    feat: Initial commit

--- azure-pipeline-creation-demo/src/main/java/com/docker/demo/DemoApplication.java
+++ azure-pipeline-creation-demo/src/main/java/com/docker/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.scope.singleton", "com.scope.prototype"})
+@ComponentScan(basePackages = {"com.scope.singleton", "com.scope.prototype","com.docker.demo"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
